084949fab2f93831335ce9c27039e7686d384829
Justin Hong <justin0404@berkeley.edu>
2022-08-01 11:13:00 -0400

    Initial Commit

---
+++ Shared/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Shared
+//
+//  Created by Justin Hong on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
